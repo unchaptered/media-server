0dadd2880d28a6b10d9b0a6c28a0cb0471232c14
Kevin-edint <workstation19961002@gmail.com>
2023-03-05 16:33:21 +0900

    Simplify : 미사용 모듈 삭제

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,9 @@
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { ScheduleModule } from '@nestjs/schedule';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+import { ConfigModule } from '@nestjs/config';
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 
 
 // Config
-import configuration from '@config/configuration';
 import { LoggerMiddleware } from '@middlewares/log.middleware';
 import { PasswordHeaderMiddleware } from '@middlewares/api.key.header.middleware';
 
@@ -21,27 +18,7 @@ import { TaskModule } from '@task/task.module';
       isGlobal: true,
       envFilePath: '.env',
       encoding: 'utf8',
-      load: [configuration]
-    }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        type: 'mysql',
-        host: configService.get<string>('AWS_RDS_HOST'),
-        port: configService.get<number>('AWS_RDS_PORT'),
-        username: configService.get<string>('AWS_RDS_USERNAME'),
-        password: configService.get<string>('AWS_RDS_PASSWORD'),
-        database: configService.get<string>('AWS_RDS_DATABASE'),
-        entities: [],
-        synchronize: configService.get<boolean>('AWS_RDS_SYNCHRONIZE'),
-        poolSize: configService.get<number>('AWS_RDS_POOL_SIZE'),
-        retryDelay: configService.get<number>('AWS_RDS_RETRY_DELY'),
-        retryAttempts: configService.get<number>('AWS_RDS_RETRY_ATTEMPTS'),
-        connectTimeout: configService.get<number>('AWS_RDS_CONNECTION_TIMEOUT'),
-        autoLoadEntities: true,
-        namingStrategy: new SnakeNamingStrategy()
-      }),
-      inject: [ConfigService]
+      load: []
     }),
     ScheduleModule.forRoot(),
     TaskModule,
--- src/config/configuration.ts
+++
@@ -1,7 +0,0 @@
-import { join } from 'path';
-import { readFileSync } from 'fs';
-
-export default (() => ({
-    JWT_PRIVATE_KEY: readFileSync(join(process.cwd(), 'private.pem'), { encoding: 'utf8' }),
-    JWT_PUBLIC_KEY: readFileSync(join(process.cwd(), 'public.pem'), { encoding: 'utf8' })
-}));
